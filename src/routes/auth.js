c84a46a301278ef6e0b56a82d34ac1e08668b147
baticlean <baticlean225@gmail.com>
2026-02-04 06:16:34 +0000

    Dashboard V1

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,27 @@
 // src/routes/auth.js
 const express = require('express');
-const { register, login, logout, getAllUsers, deleteUser } = require('../controllers/auth');
-const { protect, authorize } = require('../middleware/auth'); // On s'assure que c'est sécurisé
+const { 
+  register, 
+  login, 
+  logout, 
+  getAllUsers, // <--- On importe les nouvelles fonctions
+  deleteUser   // <--- On importe les nouvelles fonctions
+} = require('../controllers/auth');
+
+const { protect, authorize } = require('../middleware/auth'); 
 
 const router = express.Router();
 
+// Routes Publiques
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
 
-// ROUTES ADMIN (Protégées + Rôle Admin requis)
+// --- ROUTES ADMIN (ZONE INTERDITE AUX USERS) ---
+// 1. Voir la liste des utilisateurs
 router.get('/users', protect, authorize('admin'), getAllUsers);
+
+// 2. Bannir/Supprimer un utilisateur
 router.delete('/users/:id', protect, authorize('admin'), deleteUser);
 
 module.exports = router;
